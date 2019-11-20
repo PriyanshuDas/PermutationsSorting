126e0ad6f71163281e2284f49fa250453daa69c3
Priyanshu Das <das.priyanshu@gmail.com>
2019-11-20 10:50:26 +0530

    updated adjacency_calculator

--- src/permutation/adjacency_calculator.rs
+++ src/permutation/adjacency_calculator.rs
@@ -9,6 +9,7 @@ const DEBUG_ENABLED: bool = false;
 
 struct AdjacencyCalculator {
     size: usize,
+    permutations: Vec<Vec<u8>>,
     // [code][item] = mask
     mask_for_pos_with_items_greater_than_item_for_permutation: Vec<Vec<u32>>,
     // [code][pos][0] = Sum(p_i*i!), [code][pos][1] = Sum(p_i*(i+1)!),
@@ -98,22 +99,106 @@ fn get_mask_sum_of_bits_position_factorial(size: usize) -> Vec<u64> {
     masks_sum_vector
 }
 
+fn get_permutations_of_size(size: usize) -> Vec<Vec<u8>> {
+    let mut permutations_list = vec![];
+    for code in 0..constants::FACTORIALS[size] {
+        let permutation =
+            permutation_label::get_permutation_from_lehmer_code(size, code as usize);
+        permutations_list.push(permutation);
+    }
+    permutations_list
+}
+
+fn get_mask_with_bits_set_before(old_pos: i32) -> i32 {
+    if old_pos <= 0 {
+        return 0;
+    }
+    (1 << (old_pos as i32)) - 1
+}
+
 impl AdjacencyCalculator {
     pub fn init(size: usize) -> AdjacencyCalculator {
+        let permutations = get_permutations_of_size(size);
+        let mask_for_pos_with_items_greater_than_item_for_permutation =
+            get_masks_for_pos_with_items_lesser_than_item_for_permutation(size);
+        let running_sum_of_item_pos_factorial =
+            get_running_sum_of_item_pos_factorial_for_all_codes(size);
+        let mask_sum_of_bits_position_factorial = get_mask_sum_of_bits_position_factorial(size);
         return AdjacencyCalculator {
             size,
-            mask_for_pos_with_items_greater_than_item_for_permutation:
-            get_masks_for_pos_with_items_lesser_than_item_for_permutation(size),
-            running_sum_of_item_pos_factorial:
-            get_running_sum_of_item_pos_factorial_for_all_codes(size),
-            mask_sum_of_bits_position_factorial:
-            get_mask_sum_of_bits_position_factorial(size),
+            permutations,
+            mask_for_pos_with_items_greater_than_item_for_permutation,
+            running_sum_of_item_pos_factorial,
+            mask_sum_of_bits_position_factorial,
         };
     }
 
-    //todo: implement
-    pub fn get_adjacency_map(&self) {
+    //todo: can be modularized?
+    fn get_delta_for_moving_item(&self, code: u32, old_pos: usize, new_pos: i8) -> i64 {
+        let size = self.size;
+        let mut delta: i64 = 0;
+        let block_running_sum_delta = self.get_block_running_sum_delta(code, old_pos, new_pos);
+        let item_move_delta = self.get_item_move_delta(code, old_pos, new_pos);
+        let block_delta_due_to_item_move = self.get_block_delta_due_to_item_move(code, old_pos, new_pos);
+        block_running_sum_delta + item_move_delta + block_delta_due_to_item_move
+    }
 
+    //todo: complete and test
+    fn get_block_delta_due_to_item_move(&self, code: u32, old_pos: usize, new_pos: i8) -> i64 {
+        let item = self.permutations[code as usize][old_pos];
+        let lower_than_item_mask =
+            self.mask_for_pos_with_items_greater_than_item_for_permutation[code as usize][item as usize];
+
+        let block_mask =
+            get_mask_with_bits_set_before(old_pos as i32)
+                ^ get_mask_with_bits_set_before((new_pos + 1) as i32);
+
+        self.mask_sum_of_bits_position_factorial[block_mask as usize] as i64
+    }
+
+    fn get_item_move_delta(&self, code: u32, old_pos: usize, new_pos: i8) -> i64 {
+        let item_to_be_moved = self.permutations[code as usize][old_pos];
+        let lower_than_item_mask =
+            self.mask_for_pos_with_items_greater_than_item_for_permutation[code as usize][item_to_be_moved as usize];
+        let original_position_range_to_left_mask: i32 = get_mask_with_bits_set_before(old_pos as i32);
+        let new_position_to_left_mask: i32 = get_mask_with_bits_set_before(new_pos as i32);
+        let mask_for_sum_of_lower_items_to_left_original_pos =
+            (original_position_range_to_left_mask & lower_than_item_mask as i32);
+        let mask_for_sum_of_lower_items_to_left_new_pos =
+            (new_position_to_left_mask & lower_than_item_mask as i32);
+
+        self.mask_sum_of_bits_position_factorial[mask_for_sum_of_lower_items_to_left_new_pos as usize] as i64
+            - self.mask_sum_of_bits_position_factorial[mask_for_sum_of_lower_items_to_left_original_pos as usize] as i64
+    }
+
+    //todo: overflow fixes
+    fn get_block_running_sum_delta(&self, code: u32, old_pos: usize, new_pos: i8) -> i64 {
+        let block_original_right = self.running_sum_of_item_pos_factorial[code as usize][old_pos - 1][0];
+        let block_original_left =
+            if new_pos >= 0 { self.running_sum_of_item_pos_factorial[code as usize][new_pos as usize][0] } else { 0 };
+
+        let block_original_running_sum = block_original_right - block_original_left;
+
+        let block_new_right = self.running_sum_of_item_pos_factorial[code as usize][old_pos - 1][1];
+        let block_new_left =
+            if new_pos >= 0 { self.running_sum_of_item_pos_factorial[code as usize][new_pos as usize][1] } else { 0 };
+
+        let block_new_running_sum = block_new_right - block_new_left;
+
+        block_new_running_sum as i64 - block_original_running_sum as i64
+    }
+    //todo: implement
+    pub fn get_single_item_transposition_memo(&self) -> Vec<Vec<Vec<u32>>> {
+        let memo = vec![];
+        for code in 0..constants::FACTORIALS[self.size] {
+            for i in -1..(self.size as i8 - 2) {
+                for j in i + 2..self.size as i8 {
+                    let code_delta =
+                        self.get_delta_for_moving_item(code, j as usize, i);
+                }
+            }
+        }
+        memo
     }
 }
 
@@ -238,14 +323,19 @@ mod tests {
         }
 
         let test_cases = vec![
-            TestCase{ size: 1, expected_output: vec![0, 1] },
-            TestCase{ size: 2, expected_output: vec![0, 1, 1, 2] },
-            TestCase{ size: 3, expected_output: vec![0, 1, 1, 2, 2, 3, 3, 4] }
+            TestCase { size: 1, expected_output: vec![0, 1] },
+            TestCase { size: 2, expected_output: vec![0, 1, 1, 2] },
+            TestCase { size: 3, expected_output: vec![0, 1, 1, 2, 2, 3, 3, 4] }
         ];
 
         for case in test_cases {
             let output = get_mask_sum_of_bits_position_factorial(case.size);
             assert_eq!(output, case.expected_output);
         }
+    }
+
+    #[test]
+    fn test_adjacency_calculator_init() {
+        let adjacency_calculator =AdjacencyCalculator::init(12);
     }
 }
